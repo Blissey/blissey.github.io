629711bcb21ae1fe8a0c6dc709d35ad6ce47b64c
Iyer <Reese.Iyer@am.jll.com>
2016-09-24 20:04:53 -0700

    this is a comment

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
-console.log("hello world!");
-console.log("this is new3!");
+var hoursLeft = 58/60;
+var momentAtWhichhoursLeftWasCreated = Date.now();
+var currentSpeedMph = 5;
+var milesGoal = 4;
+var initialMilesTravelled = 0.1;
+
+function hoursThatHavePassed() {
+  return (Date.now()-momentAtWhichhoursLeftWasCreated)/(3600*1000);
+}
+
+function milesTravelled() {
+  return initialMilesTravelled + hoursThatHavePassed()*currentSpeedMph;
+}
+
+function requiredSpeedMph() {
+  var milesLeft = milesGoal - milesTravelled();
+  return milesLeft/hoursLeft;
+}
+
+function updateScreen() {
+    setTimeout(updateScreen, 1000);
+    document.write(requiredSpeedMph());
+    document.write("<br>");
+}
+setTimeout(updateScreen, 60);
