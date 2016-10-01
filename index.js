6e776bf39ce807cb692d96abd158715ac4560198
Iyer <Reese.Iyer@am.jll.com>
2016-10-01 16:16:36 -0700

    I learned how to use getElementById

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var hoursLeft = 58/60;
+var hoursLeft = 58/60; // this is a fraction of hours.
 var momentAtWhichhoursLeftWasCreated = Date.now();
 var currentSpeedMph = 5;
 var milesGoal = 4;
@@ -19,7 +19,9 @@ function requiredSpeedMph() {
 
 function updateScreen() {
     setTimeout(updateScreen, 1000);
-    document.write(requiredSpeedMph());
-    document.write("<br>");
+    var requiredSpeedElement = document.getElementById("RequiredSpeed");
+    requiredSpeedElement.innerText = requiredSpeedMph();
+//    document.write(requiredSpeedMph()); // don't ever use document.write.  it doesn't let us do what we need.  All it does is add.  It doesn't replace things.
+    // document.write("<br>");
 }
 setTimeout(updateScreen, 60);
